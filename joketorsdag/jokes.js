5f553fb28b1f4ac9aef7911aab4d2d165adca8fd
Joel Jonsson <jojo1109@storsjogymnasiet.se>
2023-06-08 10:57:46 +0200

    känner mig klar

--- joketorsdag/jokes.js
+++ joketorsdag/jokes.js
@@ -6,7 +6,7 @@ const p_joke = document.getElementById("pjoke")
 function getJoke(e){
 
     fetch(
-        `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`,
+        `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&type=single`,
         {
             method: "GET",
             headers: new Headers({
